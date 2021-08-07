8303a7bd75e08938a87106f71cbe8d76467a5fbc
sebastian uribe osorio <evelyn.nicolee92@gmail.com>
2021-08-07 00:22:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+    document.getElementById("excuse").innerHTML = generateExcuse();  
+};
+let generateExcuse = () => {
+
+    let pronoun = ['A', 'The'];
+    let subject = ['jogger', 'racoon', 'driver', 'comedian', 'pincone'];
+    let action = ['took my', 'threw my', 'yelled at my', 'solete my', 'bit my'];
+    let possetion = ['homewor', 'toe', 'car', 'shoe']
+    let where = ['on the street', 'in my house', 'in my driveway'];
+    
+    let proIndex = Math.floor(Math.random() * pronoun.length);
+    let subjIndex = Math.floor(Math.random() * subject.length);
+    let possetionIndex = Math.floor(Math.random() * action.length);
+    let actionIndex = Math.floor(Math.random() * possetion.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+  
+    return pronoun[proIndex] + ' ' + subject[subjIndex] + ' ' + possetion[possetionIndex] + ' ' + action[actionIndex] + ' ' + where[whereIndex];
+  }
